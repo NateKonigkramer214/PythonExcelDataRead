d2cb00e8f4cd237b56a91cfeb2399b6337945c44
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-07-24 15:47:43 +1200

    Change in data and column

--- readexcel.py
+++ readexcel.py
@@ -3,12 +3,20 @@
 Nathan Konigkramer
 
 """
+
+
 # Import the pandas
+
 import pandas as pd
+
+# Column varible 
+col = [1, 1]
 
 # Read the 'TestData.xlsx' file located at 'D:\Python_Excel\TestData.xlsx'
 # and store the data in a pandas DataFrame 
-dataframe = pd.read_excel('D:\Python_Excel\TestData.xlsx')
+dataframe = pd.read_excel('D:\Python_Excel\TestData.xlsx', usecols= col) # added the usecols to read from a column defined by var "col"
 
 # Print the contents of the DataFrame 'df'
 print(dataframe)
+
+
