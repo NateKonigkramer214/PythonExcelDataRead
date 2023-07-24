488cf55d29157ea68f1bf781b65719ecebfff4f3
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-07-24 14:49:47 +1200

    Excel Stuff

---
+++ readexcel.py
@@ -0,0 +1,14 @@
+"""
+
+Nathan Konigkramer
+
+"""
+# Import the pandas
+import pandas as pd
+
+# Read the 'TestData.xlsx' file located at 'D:\Python_Excel\TestData.xlsx'
+# and store the data in a pandas DataFrame named 'df'
+df = pd.read_excel('D:\Python_Excel\TestData.xlsx')
+
+# Print the contents of the DataFrame 'df'
+print(df)
