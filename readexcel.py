c93710ca4aa221d3eb8b0874a290b0a29f14310e
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-07-24 15:10:44 +1200

    Dataframe var change

--- readexcel.py
+++ readexcel.py
@@ -7,8 +7,8 @@ Nathan Konigkramer
 import pandas as pd
 
 # Read the 'TestData.xlsx' file located at 'D:\Python_Excel\TestData.xlsx'
-# and store the data in a pandas DataFrame named 'df'
-df = pd.read_excel('D:\Python_Excel\TestData.xlsx')
+# and store the data in a pandas DataFrame 
+dataframe = pd.read_excel('D:\Python_Excel\TestData.xlsx')
 
 # Print the contents of the DataFrame 'df'
-print(df)
+print(dataframe)
