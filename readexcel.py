9c18c5147be07586deb38d048560d2b1ebb395a7
Nathan Konigkramer <98358837+NateKonigkramer214@users.noreply.github.com>
2023-07-24 16:21:30 +1200

    Added testdata2

--- readexcel.py
+++ readexcel.py
@@ -5,8 +5,7 @@ def  read_excel(filepath, column):
     dataframe = pd.read_excel(filepath, usecols = column)
     return dataframe
 #Vars
-column = [1, 1]
-filepath = 'D:\Python_Excel\TestData.xlsx' 
+column = [0, 1]
+filepath = 'D:\Python_Excel\TestData2.xlsx' 
 dataframe = read_excel(filepath, column)
 print(dataframe)
-
